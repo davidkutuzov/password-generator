9f94249fd70ea8e2d221ef61cd613fad111c10d5
davidkutuzov <davidkutuzov@proton.me>
2026-09-18 21:36:34 +0300

    Improve input validation

--- password_generator.py
+++ password_generator.py
@@ -2,9 +2,16 @@ import secrets
 import string
 
 characters = string.ascii_letters + string.digits + string.punctuation
+try:
+    length = int(input("How long should the password be? "))
 
-length = int(input("How long should the password be? "))
-
+    if length <= 0:
+        print("Password length must be greater than 0.")
+        exit()
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
+  
 password = "".join(secrets.choice(characters) for _ in range(length))
 
 print("Your password: ", password)
