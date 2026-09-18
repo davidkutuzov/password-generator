54ebeb8323ca4e7f5d02f203a95424dd90065aa7
davidkutuzov <davidkutuzov@proton.me>
2026-09-18 21:53:32 +0300

    Refactor password generation into function

--- password_generator.py
+++ password_generator.py
@@ -1,17 +1,41 @@
 import secrets
 import string
 
-characters = string.ascii_letters + string.digits + string.punctuation
+def generate_password(length):
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    digits = string.digits
+    symbols = string.punctuation
+
+    character = lowercase + uppercase + digits + symbols
+
+    password = [
+        secrets.choice(lowercase),
+        secrets.choice(uppercase),
+        secrets.choice(digits),
+        secrets.choice(symbols)
+    ]
+    
+    password += [
+        secrets.choice(character)
+        for _ in range(length-4)
+    ]
+
+    secrets.SystemRandom().shuffle(password)
+    return "".join(password)
+
 try:
     length = int(input("How long should the password be? "))
 
     if length <= 0:
         print("Password length must be greater than 0.")
         exit()
+    if length < 8:
+        print("Password length must be at least 8.")
+        exit()
 except ValueError:
     print("Please enter a valid number.")
     exit()
   
-password = "".join(secrets.choice(characters) for _ in range(length))
-
+password = generate_password(length)
 print("Your password: ", password)
